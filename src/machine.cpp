4c15d2ee266d82a2bd89d62881da89700bc57d59
GiacomoCorradini <corra981@gmail.com>
2021-08-24 21:12:23 +0200

    impostazione file h e cpp machine

---
+++ src/machine.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <sstream>
+#include <cmath>
+#include "Car.h"
+#include "motrice.h"
+
