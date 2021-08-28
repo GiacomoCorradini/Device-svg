23aa5b87a3c1d5aa40ca4d3923a81144790446d8
GiacomoCorradini <corra981@gmail.com>
2021-08-28 11:05:03 +0200

    upload coca_init_machine

--- src/machine.cpp
+++ src/machine.cpp
@@ -6,6 +6,32 @@ using namespace std;
 #include <streambuf>
 #include <sstream>
 #include <cmath>
+
 #include "Car.h"
 #include "motrice.h"
+#include "machine.h"
 
+// funzione che scrive la strigna machine
+std::string coca_strg_machine(coca_machine* machine, int scelta, int quota){
+
+}
+
+// funzione che inizializza struct machine e parametri
+coca_machine* coca_init_machine(){
+    coca_machine* macch = new coca_machine;
+    macch->motrice = menini_init();
+    for(int i = 0; i < macch->numero; i++){
+        macch->arr_car[i] = coca_init_device();
+    }
+    return macch;
+}
+
+// funzione che legge da file machine
+coca_machine* coca_parse_machine(coca_machine* macch, std::string svg){
+
+}
+
+// funzione che setta i parametri della machine
+coca_machine* coca_myset_machine(coca_machine* macch){
+
+}
